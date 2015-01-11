f30545cbd9ee43150f82f8913be1f3128a351ef7
masweetman <slosweetman@gmail.com>
2015-01-11 13:26:01 -0800

    csv

--- app/controllers/fraternity_members_controller.rb
+++ app/controllers/fraternity_members_controller.rb
@@ -20,7 +20,7 @@ class FraternityMembersController < ApplicationController
     @fraternity_members = FraternityMember.all.sort_by{|a| [a.chapter, a.active_number]}
     export_csv = 'chapter,active_number,firstname,middlename,lastname,pledge_name,mail,phone,address,graduation_year,active' + "\n"
     @fraternity_members.each do |e|
-      export_csv += e.chapter+','+e.active_number.to_s+','+e.firstname+','+e.middlename+','+e.lastname+','+e.pledge_name+','+e.mail+','+e.phone+','+e.address+','+e.graduation_year.to_s+','+e.active.to_s + "\n"
+      export_csv += e.chapter.to_s+','+e.active_number.to_s+','+e.firstname.to_s+','+e.middlename.to_s+','+e.lastname.to_s+','+e.pledge_name.to_s+','+e.mail.to_s+','+e.phone.to_s+','+e.address.to_s+','+e.graduation_year.to_s+','+e.active.to_s + "\n"
     end
     send_data(export_csv, :type => 'text/html', :filename => "export.csv")
   end
