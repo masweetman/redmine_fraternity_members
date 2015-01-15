e9bd1f6e63be8db162c213cfe2fb49c9de018dda
Michael A. Sweetman <michael.sweetman@ago.org>
2015-01-15 15:15:56 -0800

    added filter for blank values

--- app/controllers/fraternity_members_controller.rb
+++ app/controllers/fraternity_members_controller.rb
@@ -8,10 +8,10 @@ class FraternityMembersController < ApplicationController
   def index
     sort = params[:sort]
     case sort
-    when 'firstname' then @fraternity_members = FraternityMember.all.sort_by{|a| [a.firstname.downcase]}
-    when 'lastname' then @fraternity_members = FraternityMember.all.sort_by{|a| [a.lastname.downcase]}
-    when 'pledge_name' then @fraternity_members = FraternityMember.all.sort_by{|a| [a.pledge_name.downcase]}
-    when 'zip' then @fraternity_members = FraternityMember.all.sort_by{|a| [a.address]}
+    when 'firstname' then @fraternity_members = FraternityMember.where.not(firstname: '').sort_by{|a| [a.firstname.downcase]}
+    when 'lastname' then @fraternity_members = FraternityMember.where.not(lastname: '').sort_by{|a| [a.lastname.downcase]}
+    when 'pledge_name' then @fraternity_members = FraternityMember.where.not(pledge_name: '').sort_by{|a| [a.pledge_name.downcase]}
+    when 'zip' then @fraternity_members = FraternityMember.where.not(address: '').sort_by{|a| [a.address]}
     else @fraternity_members = FraternityMember.all.sort_by{|a| [a.chapter, a.active_number]}
     end
   end
