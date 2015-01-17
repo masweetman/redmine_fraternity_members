a99310ea533218ac38cb6b031ef1644412ada7ec
masweetman <slosweetman@gmail.com>
2015-01-17 12:21:58 -0800

    moved activation to hook

--- lib/user_hooks.rb
+++ lib/user_hooks.rb
@@ -7,6 +7,10 @@ module NewUserActionsPlugin
         m = Member.new(:user => user, :roles => [Role.find_by_name('Active')])
         Project.find_by_name(user.custom_field_value(2)).members << m
       end
+      if user.custom_field_value(54).downcase == Setting.plugin_redmine_fraternity_members[:fraternity_password].downcase
+      	user.activate
+      	Mailer.account_activated(user).deliver
+      end
     end
   end
 end
