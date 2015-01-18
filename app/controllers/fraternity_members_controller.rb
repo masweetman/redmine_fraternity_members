0289d1f36f2cb57db8ac61e369e5482cf138f0fc
masweetman <slosweetman@gmail.com>
2015-01-18 11:59:24 -0800

    fixed export filter

--- app/controllers/fraternity_members_controller.rb
+++ app/controllers/fraternity_members_controller.rb
@@ -31,7 +31,22 @@ class FraternityMembersController < ApplicationController
   end
 
   def export
-    @fraternity_members = FraternityMember.all.sort_by{|a| [a.chapter, a.active_number]}
+    if params[:search].present?
+      @fraternity_members = FraternityMember.where("
+        chapter LIKE :search OR
+        active_number LIKE :search OR
+        firstname LIKE :search OR
+        lastname LIKE :search OR
+        pledge_name LIKE :search OR
+        mail LIKE :search OR
+        phone LIKE :search OR
+        address LIKE :search", :search => "#{params[:search]}%")
+    else
+      @fraternity_members = FraternityMember.all
+    end
+
+    @fraternity_members = @fraternity_members.sort_by{|a| [a.chapter, a.active_number]}
+
     export_csv = 'chapter,active_number,firstname,middlename,lastname,pledge_name,mail,phone,address,graduation_year,active' + "\n"
     @fraternity_members.each do |e|
       export_csv += '"'+e.chapter.to_s+'"'+','+
