8035c3718bdd889a60f58370c78077b1cad1e1c9
Michael A. Sweetman <michael.sweetman@ago.org>
2015-01-21 19:13:07 -0800

    removed require

--- lib/mailer_patch.rb
+++ lib/mailer_patch.rb
@@ -8,26 +8,26 @@ module MailerPatch
 			unloadable
 			belongs_to :deliverable
 
-			#alias_method_chain :news_added, :all_actives
+			alias_method_chain :news_added, :all_actives
 		end
 	end
 
 	module InstanceMethods
 
-	#	def news_added_with_all_actives(news)
-	#		actives = news.recipients
-	#		while actives.count > 0 do
-	#			mail_batch = actives.pop(99)
+		def news_added_with_all_actives(news)
+			actives = news.recipients
+			while actives.count > 0 do
+				mail_batch = actives.pop(99)
 
-	#			redmine_headers 'Project' => news.project.identifier
-	#			@author = news.author
-	#			message_id news
-	#			references news
-	#			@news = news
-	#			@news_url = url_for(:controller => 'news', :action => 'show', :id => news)
-	#			mail(:to => ["slosweetman@gmail.com"], :subject => "[#{news.project.name}] #{l(:label_news)}: #{news.title}").deliver
-	#		end
-	#	end
+				redmine_headers 'Project' => news.project.identifier
+				@author = news.author
+				message_id news
+				references news
+				@news = news
+				@news_url = url_for(:controller => 'news', :action => 'show', :id => news)
+				mail(:to => ["slosweetman@gmail.com"], :subject => "[#{news.project.name}] #{l(:label_news)}: #{news.title}").deliver
+			end
+		end
 
 	end
 end
