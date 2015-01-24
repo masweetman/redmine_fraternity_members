a47234672f7940572d92c112a1be17a2500a86e9
masweetman <slosweetman@gmail.com>
2015-01-24 09:25:30 -0800

    simplified html/doc export

--- app/controllers/shingle_controller.rb
+++ app/controllers/shingle_controller.rb
@@ -4,43 +4,34 @@ class ShingleController < ApplicationController
         def shingle_export
                 shingle = Issue.find(params[:id])
 
-shingle_html = '<html xmlns:v="urn:schemas-microsoft-com:vml"'
-shingle_html += 'xmlns:o="urn:schemas-microsoft-com:office:office"'
-shingle_html += 'xmlns:w="urn:schemas-microsoft-com:office:word"'
-shingle_html += 'xmlns:m="http://schemas.microsoft.com/office/2004/12/omml"'
-shingle_html += 'xmlns="http://www.w3.org/TR/REC-html40">'
-shingle_html += '<head>'
-shingle_html += '<meta http-equiv=Content-Type content="text/html;charset=UTF-8">'
-shingle_html += '<meta name=ProgId content=Word.Document>'
-shingle_html += '<meta name=Generator content="Microsoft Word 14">'
-shingle_html += '<meta name=Originator content="Microsoft Word 14">'
-shingle_html += '<style>'
-shingle_html += '</style>'
-shingle_html += '</head>'
-shingle_html += '<body lang=EN-US style=\'tab-interval:36.0pt;font-family:"Times New Roman","serif"\'>'
-shingle_html += '<div>'
-shingle_html += '<p align=center>'
-shingle_html += '<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>'
-shingle_html += '<span style=\'font-size:36.0pt\'>'
-shingle_html += shingle.custom_field_value(83)
-shingle_html += '</span>'
-shingle_html += '<br><br><br><br><br><br><br><br>'
-shingle_html += '<i><span style=\'font-size:17.0pt\'>'
-shingle_html += 'Member Number '
-shingle_html += shingle.custom_field_value(24)
-shingle_html += ', ' + shingle.project.name + ' Chapter'
-shingle_html += '<br><br>'
-shingle_html += 'Initiated on '
-shingle_html += shingle.custom_field_value(23)
-shingle_html += ' at '
-shingle_html += shingle.custom_field_value(97)
-shingle_html += '</span></i>'
-shingle_html += '</p>'
-shingle_html += '</div>'
-shingle_html += '</body>'
-shingle_html += '</html>'
+shingle_html = '<html xmlns:v="urn:schemas-microsoft-com:vml"
+				xmlns:o="urn:schemas-microsoft-com:office:office"
+				xmlns:w="urn:schemas-microsoft-com:office:word"
+				xmlns:m="http://schemas.microsoft.com/office/2004/12/omml"
+				xmlns="http://www.w3.org/TR/REC-html40">
+				<head>
+				<meta http-equiv=Content-Type content="text/html;charset=UTF-8">
+				<meta name=ProgId content=Word.Document>
+				<meta name=Generator content="Microsoft Word 14">
+				<meta name=Originator content="Microsoft Word 14">
+				<style>
+				</style>
+				</head>
+				<body lang=EN-US style=\'tab-interval:36.0pt;font-family:"Times New Roman","serif"\'>
+				<div>
+				<p align=center>
+				<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+				<span style=\'font-size:36.0pt\'>'
+shingle_html += shingle.custom_field_value(83).to_s
+shingle_html += '</span>
+				<br><br><br><br><br><br><br><br>
+				<i><span style=\'font-size:17.0pt\'>
+				Member Number '
+shingle_html += shingle.custom_field_value(24).to_s + ', '
+shingle_html += shingle.project.name.to_s + ' Chapter' + '<br><br> Initiated on '
+shingle_html += shingle.custom_field_value(23).to_s + ' at '
+shingle_html += shingle.custom_field_value(97).to_s + '</span></i> </p> </div> </body> </html>'
 
-				
                 send_data(shingle_html, :type => 'text/doc',
                 	:filename => shingle.project.name.to_s + '_' + shingle.custom_field_value(24).to_s + '_shingle.doc')
         end
