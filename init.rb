7cd889f497228c96404008f82bbbd44ea81c245b
masweetman <slosweetman@gmail.com>
2015-01-30 20:48:58 -0600

    fixed wiki link

--- init.rb
+++ init.rb
@@ -15,7 +15,7 @@ Redmine::Plugin.register :redmine_fraternity_members do
   directory_path = redmine_root + '/directory'
 
   menu :top_menu, :fraternity_members, { :controller => 'fraternity_members', :action => 'index'}, :caption => 'Member Directory'
-  menu :top_menu, :wiki, { :controller => 'wiki', :action => 'show', :project_id => 'chapters' }, :caption => 'National Wiki'
+  menu :top_menu, :wiki, { :controller => 'wiki', :action => 'show', :project_id => 'chapters', :id => 'wiki' }, :caption => 'National Wiki'
   menu :top_menu, :news, { :controller => 'news', :action => 'index', :project_id => 'chapters' }, :caption => 'National News'
 
   settings :default => {:actives_only => ""}, :partial => 'shared/settings'
