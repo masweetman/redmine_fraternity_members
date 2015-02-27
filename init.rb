9f667577a7e29200f14db43c9cf815ff52a5ff34
masweetman <slosweetman@gmail.com>
2015-02-27 15:13:09 -0800

    release 1.0.0

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_fraternity_members do
   name 'Redmine Fraternity Members'
   author 'Mike Sweetman'
   description 'This plugin manages the member database.'
-  version '0.0.1'
+  version '1.0.0'
   url 'https://github.com/masweetman/redmine_fraternity_members.git'
   author_url ''
 
