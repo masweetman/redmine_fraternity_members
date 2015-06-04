10d33541c93ff6b4d8d5ca9cccc5e82c7058aa39
masweetman <slosweetman@gmail.com>
2015-06-04 01:35:13 -0700

    completed revenue and cash flow

--- app/controllers/budget_actuals_controller.rb
+++ app/controllers/budget_actuals_controller.rb
@@ -10,14 +10,26 @@ class BudgetActualsController < ApplicationController
 		budgetCategories = CustomField.find(98).possible_values
 		@project = Project.find(params[:id])
 		@expenses = @project.issues.where('tracker_id = ? AND created_on > ?', 22, Date.today - 1.year)
+		@deposits = @project.issues.where('tracker_id = ? AND created_on > ?', 21, Date.today - 1.year)
 		@latestBudget = @project.issues.where(:tracker_id => 19).last
 
 		dates = []
 		@dateStrings = []
 		@annualExpenses = {}
+		@annualDeposits = []
+
 		for i in 0..11
 			dates[i] = Date.today - i.month
 			@dateStrings[i] = dates[i].strftime('%b %y')
+		end
+
+		for d in @deposits
+			for i in 0..11
+				if Date.parse(d.custom_field_value(19)).month == dates[i].month &&
+				   Date.parse(d.custom_field_value(19)).year == dates[i].year then
+					@annualDeposits[i] = @annualDeposits[i].to_f + d.custom_field_value(94).to_f
+				end
+			end
 		end
 				
 		for e in @expenses
