021fc9af11464d09b374a19e809a673700cf0871
masweetman <slosweetman@gmail.com>
2015-06-12 22:19:05 -0700

    completed deposit index

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -5,4 +5,18 @@ class DepositsController < ApplicationController
 	helper :custom_fields
 	include CustomFieldsHelper
 
+	def index
+		@project = Project.find(params[:id])
+
+		@checks = @project.issues.where('tracker_id = ? AND created_on > ?', 26, Date.today - 2.year)
+
+		@deposits = {}
+
+		for c in @checks
+			@deposits[c.custom_field_value(105)] = @deposits[c.custom_field_value(105)].to_f + c.custom_field_value(104).to_f
+		end
+
+		@deposits = @deposits.sort.reverse
+	end
+
 end
