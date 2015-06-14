2bce834c5fe1fa64f2fb54bd1933da20e79b47fd
masweetman <slosweetman@gmail.com>
2015-06-13 23:16:09 -0700

    v1 of email groups

---
+++ app/controllers/email_groups_controller.rb
@@ -0,0 +1,90 @@
+class EmailGroupsController < ApplicationController
+	#unloadable
+	helper :sort
+	include SortHelper
+	helper :custom_fields
+	include CustomFieldsHelper
+
+	def index
+
+		#national council members
+		@national_council = Project.find(6).users
+		for user in @national_council
+			if (user.roles_for_project(Project.find(6)).first.id == 36 ||
+				user.roles_for_project(Project.find(6)).first.id == 39) &&
+				user.roles_for_project(Project.find(6)).length == 1
+				@national_council -= [user]
+			end
+		end
+
+		@advisors = []
+		@presidents = []
+		@vice_presidents = []
+		@chaplains = []
+		@pledgemasters = []
+		@treasurers = []
+		@house_managers = []
+		@secretaries =[]
+		@social_media_managers = []
+		@actives = []
+		@agodelphian_editors = []
+
+		for chapter in Project.where(:parent_id => 6)
+			for user in chapter.users
+				case
+				when user.roles_for_project(chapter).include?(Role.find_by_name("Chapter Advisor"))
+					@advisors << user
+				when user.roles_for_project(chapter).include?(Role.find_by_name("President"))
+					@presidents << user
+				when user.roles_for_project(chapter).include?(Role.find_by_name("Vice President"))
+					@vice_presidents << user
+				when user.roles_for_project(chapter).include?(Role.find_by_name("Chaplain"))
+					@chaplains << user
+				when user.roles_for_project(chapter).include?(Role.find_by_name("Pledgemaster"))
+					@pledgemasters << user
+				when user.roles_for_project(chapter).include?(Role.find_by_name("Treasurer"))
+					@treasurers << user
+				when user.roles_for_project(chapter).include?(Role.find_by_name("House Manager"))
+					@house_managers << user
+				when user.roles_for_project(chapter).include?(Role.find_by_name("Secretary"))
+					@secretaries << user
+				when user.roles_for_project(chapter).include?(Role.find_by_name("Social Media Manager"))
+					@social_media_managers << user
+				when user.roles_for_project(chapter).include?(Role.find_by_name("Active"))
+					@actives << user
+				when user.roles_for_project(chapter).include?(Role.find_by_name("AGODelphian Editor"))
+					@agodelphian_editors << user
+				end
+			end
+		end
+
+		@advisors += @national_council
+		@agodelphian_editors += @advisors
+		@presidents += @advisors
+		@vice_presidents += @presidents
+		@chaplains += @presidents
+		@pledgemasters += @presidents
+		@treasurers += @presidents
+		@house_managers += @presidents
+		@secretaries += @presidents
+		@social_media_managers += @presidents
+		@officers = @vice_presidents + @chaplains + @pledgemasters + @treasurers + @house_managers + @secretaries
+		@actives += @officers + @social_media_managers
+		
+		@national_council = @national_council.uniq
+		@advisors = @advisors.uniq
+		@agodelphian_editors = @agodelphian_editors.uniq
+		@presidents = @presidents.uniq
+		@vice_presidents = @vice_presidents.uniq
+		@chaplains = @chaplains.uniq
+		@pledgemasters = @pledgemasters.uniq
+		@treasurers = @treasurers.uniq
+		@house_managers = @house_managers.uniq
+		@secretaries = @secretaries.uniq
+		@social_media_managers = @social_media_managers.uniq
+		@officers = @officers.uniq
+		@actives = @actives.uniq
+
+
+	end
+end
--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_fraternity_members do
   name 'Redmine Fraternity Members'
   author 'Mike Sweetman'
   description 'This plugin manages the member database.'
-  version '2.1.2'
+  version '3.0.0'
   url 'https://github.com/masweetman/redmine_fraternity_members.git'
   author_url ''
 
@@ -23,6 +23,7 @@ Redmine::Plugin.register :redmine_fraternity_members do
   permission :view_budget_actuals, { :budget_actuals => :index }
 
   menu :top_menu, :fraternity_members, { :controller => 'fraternity_members', :action => 'index' }, :caption => 'Member Directory'
+  menu :top_menu, :email_groups, { :controller => 'email_groups', :action => 'index' }, :caption => 'Email Groups'
   menu :top_menu, :wiki, { :controller => 'wiki', :action => 'show', :project_id => 'chapters', :id => 'wiki' }, :caption => 'National Wiki'
   menu :top_menu, :news, { :controller => 'news', :action => 'index', :project_id => 'chapters' }, :caption => 'National News'
   menu :project_menu, :budget_actuals, { :controller => 'budget_actuals', :action => 'index' }, :caption => 'Budget', :after => :activity
