9b17aca77e13e1814a18f77cdfaba7a01d1f0f49
masweetman <slosweetman@gmail.com>
2015-06-15 13:24:38 -0700

    completed authentication test

--- lib/google_admin_directory_wrapper.rb
+++ lib/google_admin_directory_wrapper.rb
@@ -1,21 +1,21 @@
 class GoogleAdminDirectoryWrapper
 
-	def initialize
+	def new
 		@client = Google::APIClient.new(
 			:application_name => 'Redmine Fraternity Members',
 			:application_version => '3.0.1'
 		)
 		@directory = @client.discovered_api('admin', 'directory_v1')
-		@client_secrets = Google::APIClient::ClientSecrets.load("#{Rails.root}/config/client_secrets.json")
-		@flow = Google::APIClient::InstalledAppFlow.new(
-			:client_id => @client_secrets.client_id,
-			:client_secret => @client_secrets.client_secret,
-			:scope => ['https://www.googleapis.com/auth/admin.directory.group']
-		)
-		@client.authorization = @flow.authorize
-		result = @client.execute(:api_method => @directory.groups.list)
-
-		puts result.data
+		google_admin = Setting.plugin_redmine_fraternity_members['google_admin_email']
+		key = Google::APIClient::KeyUtils.load_from_pkcs12("#{Rails.root}/config/client.p12", 'notasecret')
+		@client.authorization = Signet::OAuth2::Client.new(
+		  :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+		  :audience => 'https://accounts.google.com/o/oauth2/token',
+		  :person => google_admin,
+		  :scope => 'https://www.googleapis.com/auth/admin.directory.group',
+		  :issuer => '711803943574-27k44uab2s4mp3a15h80oj5eced370a1@developer.gserviceaccount.com',
+		  :signing_key => key)
+		@client.authorization.fetch_access_token!
 
 		@email_addresses = Setting.plugin_redmine_fraternity_members['email_addresses']
 	end
