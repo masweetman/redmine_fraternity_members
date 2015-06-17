2205ed5bc278f0c173d25826e3161e1aed727629
masweetman <slosweetman@gmail.com>
2015-06-16 21:36:47 -0700

    added links for group members

--- app/controllers/email_groups_controller.rb
+++ app/controllers/email_groups_controller.rb
@@ -8,4 +8,19 @@ class EmailGroupsController < ApplicationController
 	def index
 		@chapters = Project.where(:parent_id => 6).sort
 	end
+
+	def show
+		@group = params[:group]
+		@chapter = params[:chapter]
+
+		if @chapter.nil?
+			@email = Setting.plugin_redmine_fraternity_members['email_addresses'][@group]
+		else
+			@email = Setting.plugin_redmine_fraternity_members[@chapter + '_email_addresses'][@group]
+		end
+
+		google_directory = GoogleDirectory.new
+		@members = google_directory.list_members(@email)
+		
+	end
 end
