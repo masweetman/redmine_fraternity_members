c89b5f2926638ce7fc537e4113f02f9440954183
masweetman <slosweetman@gmail.com>
2015-06-17 21:36:16 -0700

    removed obsolte mailer and news patches; replaced by google groups

--- init.rb
+++ init.rb
@@ -10,14 +10,12 @@ require 'user_hooks'
 
 require_dependency 'registration_patch'
 require_dependency 'user_patch'
-require_dependency 'news_patch'
-require_dependency 'mailer_patch'
 
 Redmine::Plugin.register :redmine_fraternity_members do
   name 'Redmine Fraternity Members'
   author 'Mike Sweetman'
   description 'This plugin manages the member database.'
-  version '3.1.0'
+  version '3.2.0'
   url 'https://github.com/masweetman/redmine_fraternity_members.git'
   author_url ''
 
