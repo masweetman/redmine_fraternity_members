adb43e2947c05cb3391bf1919427d8214a65a359
Mike Sweetman <mike@michaelsweetman.com>
2017-09-23 08:38:20 -0700

    fixed issue with changes to issue descriptions

---
+++ db/migrate/010_convert_database_to_utf8mb4.rb
@@ -0,0 +1,11 @@
+class ConvertDatabaseToUtf8mb4 < ActiveRecord::Migration
+  def change
+    # for each table that will store unicode execute:
+    execute "ALTER TABLE issues CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    execute "ALTER TABLE journal_details CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    # for each string/text column with unicode content execute:
+    execute "ALTER TABLE issues MODIFY `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+    execute "ALTER TABLE journal_details MODIFY `old_value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+    execute "ALTER TABLE journal_details MODIFY `value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+  end
+end
