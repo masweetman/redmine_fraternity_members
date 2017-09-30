607f5ae5362cfbb5f9fcd70c97d385be048ccd6a
masweetman <slosweetman@gmail.com>
2017-09-29 20:27:32 -0700

    updated shingles

--- app/controllers/shingle_controller.rb
+++ app/controllers/shingle_controller.rb
@@ -6,85 +6,43 @@ class ShingleController < ApplicationController
 
 	def index
 	  @project = Project.find(6)
-	  @shingles = Issue.where("tracker_id = 7 AND status_id <> 9").order("project_id")
+	  @shingles = Issue.where("tracker_id = 30 AND status_id <> 9")
 	end
 
 	def mark_all_as_shipped
 	  @project = Project.find(6)
-          shingles = Issue.where("tracker_id = 7 AND status_id <> 9").order("project_id")
-          shingles.each do |shingle|
-            shingle.status_id = 9
-            shingle.save
-          end
-          redirect_to controller: "shingle", action: "index"
-          flash[:notice] = l(:notice_successful_update)
-        end
+    shingles = Issue.where("tracker_id = 30 AND status_id <> 9")
+    shingles.each do |shingle|
+      shingle.status_id = 9
+      shingle.save
+    end
+    redirect_to controller: "shingle", action: "index"
+    flash[:notice] = l(:notice_successful_update)
+  end
 
-        def mark_as_shipped
+  def mark_as_shipped
 	  @project = Project.find(6)
 	  shingle = Issue.find(params[:shingle])
-          shingle.status_id = 9
-          shingle.save
-          redirect_to controller: "shingle", action: "index"
-          flash[:notice] = l(:notice_successful_update)
-        end
+    shingle.status_id = 9
+    shingle.save
+    redirect_to controller: "shingle", action: "index"
+    flash[:notice] = l(:notice_successful_update)
+  end
 
 	def invoices_export_pdf
 		send_data(shingles_invoices_pdf, :type => 'application/pdf',
-                :filename => 'Unshipped_Shingles_Invoices.pdf')
+      :filename => 'Unshipped_Shingles_Invoices.pdf')
 	end
 
-        def shingle_export_pdf
-    		shingle = Issue.find(params[:id])
-			send_data(shingle_to_pdf(shingle), :type => 'application/pdf',
-            	:filename => shingle.project.name.to_s + '_' + shingle.custom_field_value(24).to_s + '_shingle.pdf')
-        end
+  def new_shingle_export_pdf
+    shingle = Issue.find(params[:id])
+    send_data(new_shingle_to_pdf(shingle), :type => 'application/pdf',
+      :filename => shingle.custom_field_value(134).to_s + '_' + shingle.custom_field_value(136).to_s.gsub('/', '-') + '_shingles.pdf')
+  end
 
-        def shingles_export_pdf
-			send_data(shingles_to_pdf, :type => 'application/pdf',
-            	:filename => 'Unshipped_Shingles.pdf')
-        end
-
-        def shingle_export_html
-	        shingle = Issue.find(params[:id])
-	        initiation_date = Date.parse(shingle.custom_field_value(23).to_s)
-	        initiation_date = initiation_date.strftime('%B %-d, %Y')
-
-			shingle_html = '<html xmlns:v="urn:schemas-microsoft-com:vml"
-							xmlns:o="urn:schemas-microsoft-com:office:office"
-							xmlns:w="urn:schemas-microsoft-com:office:word"
-							xmlns:m="http://schemas.microsoft.com/office/2004/12/omml"
-							xmlns="http://www.w3.org/TR/REC-html40">
-							<head>
-							<meta http-equiv=Content-Type content="text/html;charset=UTF-8">
-							<meta name=ProgId content=Word.Document>
-							<meta name=Generator content="Microsoft Word 14">
-							<meta name=Originator content="Microsoft Word 14">
-							<style>
-							p.big {
-								line-height: 2425%;
-							}
-							</style>
-							</head>
-							<body lang=EN-US style=\'tab-interval:34.0pt;font-family:"Times New Roman","serif"\'>
-							<div>
-							<p align=center class=\'big\'>
-							<br>
-							</p>
-							<p align=center>
-							<span style=\'font-size:34.0pt\'>'
-			shingle_html += shingle.custom_field_value(83).to_s
-			shingle_html += '</span>
-							<br><br><br><br><br><br><br><br>
-							<i><span style=\'font-size:16.0pt\'>
-							Member Number '
-			shingle_html += shingle.custom_field_value(24).to_s + ', '
-			shingle_html += shingle.project.name.to_s + ' Chapter' + '<br>Initiated on '
-			shingle_html += initiation_date + ' at<br>'
-			shingle_html += shingle.custom_field_value(97).to_s + '</span></i></p></div></body></html>'
-
-	        send_data(shingle_html, :type => 'text/doc',
-	        	:filename => shingle.project.name.to_s + '_' + shingle.custom_field_value(24).to_s + '_shingle.doc')
-        end
+  def shingles_export_pdf
+    send_data(new_shingles_to_pdf, :type => 'application/pdf',
+      :filename => 'Unshipped_Shingles.pdf')
+  end
 
 end
--- lib/redmine/export/pdf/shingle_pdf_helper.rb
+++ lib/redmine/export/pdf/shingle_pdf_helper.rb
@@ -2,134 +2,141 @@ module Redmine
   module Export
     module PDF
       module ShinglePdfHelper
-        
-        # Returns a PDF string of a single shingle
-        def shingle_to_pdf(shingle)
-          member_name = shingle.custom_field_value(83).to_s
-          member_number = shingle.custom_field_value(24).to_s
-          chapter = shingle.project.name.to_s
-          initiation_date = Date.parse(shingle.custom_field_value(23).to_s).strftime('%B %-d, %Y')
-          university = shingle.custom_field_value(97).to_s
 
-          # [member_name]
-          # Member Number [member_number], [chapter] Chapter 
-          # Initiated on [initiation_date] at
-          # [university]
-
+        def new_shingles_to_pdf
           pdf = RBPDF.new
-          font = 'times'
-          pdf.set_print_header(false)
-          pdf.set_print_footer(false)
-          pdf.set_margins(10, 148, 10)
-          pdf.add_page('P','LETTER',true,false)
-
-          pdf.set_font(font,'', 34)
-
-          pdf.write(5, member_name, '', 0, 'C', true)
-
-          pdf.set_font(font,'I', 16)
-          
-          pdf.ln(32)
-
-          pdf.write(5, "Member Number " + member_number + ", " + chapter + " Chapter", '', 0, 'C', true)
-          pdf.write(5, "Initiated on " + initiation_date  + " at", '', 0, 'C', true)
-          pdf.write(5, university, '', 0, 'C', true)
-
-          pdf.output
- 
-        end
-
-        # Returns a PDF string of all open shingles
-        def shingles_to_pdf
-          pdf = RBPDF.new
-          font = 'times'
           pdf.set_print_header(false)
           pdf.set_print_footer(false)
           pdf.set_margins(10, 148, 10)
 
-          Issue.where("tracker_id = 7 AND status_id <> 9").order("project_id").each do |shingle|
-            #if shingle.project.parent_id == 6
+          Issue.where("tracker_id = 30 AND status_id <> 9").each do |shingle|
+            names = shingle.description.split /[\r\n]+/
+            names.shift if names.first == ""
+            names.shift if names.first == "Full name for shingle (31 character max)"
+            names.shift if names.first == "Active Number (integer only)"
+            members = names.each_slice(2).to_a
 
-              member_name = shingle.custom_field_value(83).to_s
-              member_number = shingle.custom_field_value(24).to_s
-              chapter = shingle.project.name.to_s
-              initiation_date = Date.parse(shingle.custom_field_value(23).to_s).strftime('%B %-d, %Y')
-              university = shingle.custom_field_value(97).to_s
+            chapter = shingle.custom_field_value(134).to_s
+            university = shingle.custom_field_value(135).to_s
+            initiation_date = Date.strptime(shingle.custom_field_value(136).to_s, "%m/%d/%Y").strftime('%B %-d, %Y')
 
-              pdf.add_page('P','LETTER',true,false)
-              pdf.set_font(font,'', 34)
-              pdf.write(5, member_name, '', 0, 'C', true)
-              pdf.set_font(font,'I', 16)
-              pdf.ln(32)
-              pdf.write(5, "Member Number " + member_number + ", " + chapter + " Chapter", '', 0, 'C', true)
-              pdf.write(5, "Initiated on " + initiation_date  + " at", '', 0, 'C', true)
-              pdf.write(5, university, '', 0, 'C', true)
-
-            #end
+            members.each do |member|
+              member_name = member[0].gsub('&quot;', "\"")
+              member_number = member[1].gsub(/[^0-9]/, '')
+              shingle_page(pdf, member_name, member_number, chapter, initiation_date, university)
+            end
           end
 
           pdf.output
- 
+        end
+        
+        def new_shingle_to_pdf(shingle)
+          pdf = RBPDF.new
+          pdf.set_print_header(false)
+          pdf.set_print_footer(false)
+          pdf.set_margins(10, 148, 10)
+
+          names = shingle.description.split /[\r\n]+/
+          names.shift if names.first == ""
+          names.shift if names.first == "Full name for shingle (31 character max)"
+          names.shift if names.first == "Active Number (integer only)"
+          members = names.each_slice(2).to_a
+
+          chapter = shingle.custom_field_value(134).to_s
+          university = shingle.custom_field_value(135).to_s
+          initiation_date = Date.strptime(shingle.custom_field_value(136).to_s, "%m/%d/%Y").strftime('%B %-d, %Y')
+
+          members.each do |member|
+            member_name = member[0].gsub('&quot;', "\"")
+            member_number = member[1].gsub(/[^0-9]/, '')
+            shingle_page(pdf, member_name, member_number, chapter, initiation_date, university)
+          end
+
+          pdf.output
+        end
+
+        def shingle_page(pdf, member_name, member_number, chapter, initiation_date, university)
+          font = 'times'
+          pdf.add_page('P','LETTER',true,false)
+          pdf.set_font(font,'', 34)
+          pdf.write(5, member_name, '', 0, 'C', true)
+          pdf.set_font(font,'I', 16)
+          pdf.ln(32)
+          pdf.write(5, "Member Number " + member_number + ", " + chapter + " Chapter", '', 0, 'C', true)
+          pdf.write(5, "Initiated on " + initiation_date  + " at", '', 0, 'C', true)
+          pdf.write(5, university, '', 0, 'C', true)
         end
 
         def shingles_invoices_pdf
           pdf = RBPDF.new
-          font = 'times'
           pdf.set_print_header(false)
           pdf.set_print_footer(false)
           pdf.set_margins(15, 20, 15)
 
-          shingles = Issue.where("tracker_id = 7 AND status_id <> 9").order("project_id")
-          shingles = shingles.joins('INNER JOIN custom_values ON (issues.id = custom_values.customized_id)').where('custom_values.custom_field_id = 121') 
-
-          shingles.group('custom_values.value').count.each do |ship_address|
-
-            invoice_shingles = shingles.where('custom_values.value = ?', ship_address[0])
-            chapter = invoice_shingles.last.project.name
+          issues = Issue.where("tracker_id = 30 AND status_id <> 9")
+          issues = issues.joins('INNER JOIN custom_values ON (issues.id = custom_values.customized_id)').where('custom_values.custom_field_id = 121')
+          
+          issues.group('custom_values.value').count.each do |ship_address|
+            invoice_shingles = issues.where('custom_values.value = ?', ship_address[0])
+            chapter = invoice_shingles.last.custom_field_value(134).to_s
             address = Setting.plugin_redmine_fraternity_members['address'].to_s
-            ship_to_name = invoice_shingles.last.author.name
-            ship_to_address = ship_address[0]
+            ship_to = ship_address[0].split ("; ")
+            ship_to.delete ""
+            ship_to_name = ship_to.shift
+            ship_to_address = ship_to.join("\r\n")
             date = Date.today.strftime('%B %-d, %Y')
 
-            pdf.add_page('P','LETTER',true,false)
-
-            pdf.set_font(font,'B', 14)
-            pdf.cell(50, 0, "Alpha Gamma Omega", 0, 0, 'L', 0)
-            pdf.cell(0, 0, "SHINGLE INVOICE", 0, 0, 'R', 0)
-            pdf.ln(6)
-
-            pdf.set_font(font,'', 12)
-            pdf.write(5, address, '', 0, 'L', true)
-            pdf.ln(10)
-
-            pdf.set_font(font,'', 12)
-            pdf.write(5, "Date: " + date, '', 0, 'L', true)
-            pdf.ln(5)
-
-            pdf.set_font(font,'B', 12)
-            pdf.write(5, "Ship to", '', 0, 'L', true)
-            pdf.set_font(font,'', 12)
-            pdf.write(5, chapter + " Chapter", '', 0, 'L', true)
-            pdf.write(5, "ATTN: " + ship_to_name, '', 0, 'L', true)
-            pdf.write(5, ship_to_address, '', 0, 'L', true)
-            pdf.ln(10)
-
-            pdf.set_font(font,'B', 12)
-            pdf.cell(24, 0, "Active No.", 0, 0, 'L', 0)
-            pdf.cell(0, 0, "Name", 0, 0, 'L', 0)
-            pdf.ln(5)
-
-            pdf.set_font(font,'', 12)
+            members = []
             invoice_shingles.each do |shingle|
-                pdf.cell(24, 0, shingle.custom_field_value(24), 0, 0, 'L', 0)
-                pdf.cell(0, 0, shingle.custom_field_value(83), 0, 0, 'L', 0)
-                pdf.ln(5)
+              names = shingle.description.split /[\r\n]+/
+              names.shift if names.first == ""
+              names.shift if names.first == "Full name for shingle (31 character max)"
+              names.shift if names.first == "Active Number (integer only)"
+              members += names.each_slice(2).to_a
             end
 
+            invoice_page(pdf, address, date, chapter, ship_to_name, ship_to_address, members)
           end
 
           pdf.output
+        end
 
+        def invoice_page(pdf, address, date, chapter, ship_to_name, ship_to_address, members)
+          font = 'times'
+          pdf.add_page('P','LETTER',true,false)
+
+          pdf.set_font(font,'B', 14)
+          pdf.cell(50, 0, "Alpha Gamma Omega", 0, 0, 'L', 0)
+          pdf.cell(0, 0, "SHINGLE INVOICE", 0, 0, 'R', 0)
+          pdf.ln(6)
+
+          pdf.set_font(font,'', 12)
+          pdf.write(5, address, '', 0, 'L', true)
+          pdf.ln(10)
+
+          pdf.set_font(font,'', 12)
+          pdf.write(5, "Date: " + date, '', 0, 'L', true)
+          pdf.ln(5)
+
+          pdf.set_font(font,'B', 12)
+          pdf.write(5, "Ship to", '', 0, 'L', true)
+          pdf.set_font(font,'', 12)
+          pdf.write(5, chapter + " Chapter", '', 0, 'L', true)
+          pdf.write(5, "ATTN: " + ship_to_name, '', 0, 'L', true)
+          pdf.write(5, ship_to_address, '', 0, 'L', true)
+          pdf.ln(10)
+
+          pdf.set_font(font,'B', 12)
+          pdf.cell(24, 0, "Active No.", 0, 0, 'L', 0)
+          pdf.cell(0, 0, "Name", 0, 0, 'L', 0)
+          pdf.ln(5)
+
+          pdf.set_font(font,'', 12)
+          members.each do |member|
+            pdf.cell(24, 0, member[1].gsub(/[^0-9]/, ''), 0, 0, 'L', 0)
+            pdf.cell(0, 0, member[0].gsub('&quot;', "\""), 0, 0, 'L', 0)
+            pdf.ln(5)
+          end
         end
 
       end
