8b5b2eff8a9ea8cbff54a7790a7ad8d510dbc9e1
Mike Sweetman <mike@michaelsweetman.com>
2017-10-04 08:34:25 -0700

    added sorting

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -4,11 +4,28 @@ class HistoryController < ApplicationController
   include SortHelper
 
   def index
-    sort_init [['added_on', 'desc'], ['role', 'asc']]
-    sort_update %w(added_on removed_on role user)
+    sort_init [['added_on', 'desc'], ['role_id', 'asc']]
+    sort_update %w(added_on removed_on role_id user_id)
 
     @project = Project.find(params[:id])
-    @historical_roles = HistoricalRole.where(project_id: @project.id)
+    scope = HistoricalRole.where(project_id: @project.id)
+    @roles = Role.all.sort_by{ |r| r.name }
+    include_role_ids = [] 
+    scope.map{ |hr| include_role_ids << hr.role_id }
+    Role.all.each do |r|
+      @roles.delete(r) unless include_role_ids.include? r.id
+    end
+
+    if params['role']
+      scope = scope.where(role_id: params['role'])
+      @selected_role = params['role']
+    end
+
+    count_per_page = 20
+    @role_count = scope.count
+    @role_pages = Paginator.new @role_count, count_per_page, params['page']
+    @offset ||= @role_pages.offset
+    @historical_roles = scope.offset(@offset).limit(count_per_page).order(sort_clause)
   end
 
 end
